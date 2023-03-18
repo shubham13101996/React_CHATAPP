e6e2add27258d692fe30fdcd20176cf4af888773
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-03-18 16:25:54 +0530

    create context and provider

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
